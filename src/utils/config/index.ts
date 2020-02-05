af46db6d1fa5b75f44136f20cda508993ca23cf4
soichiro-nitta <soic1226@gmail.com>
2020-02-05 20:36:43 +0900

    Add: utils/config

---
+++ src/utils/config/index.ts
@@ -0,0 +1,30 @@
+export default {
+  url: {
+    facebook: 'https://facebook.com/xxx',
+    instagram: 'https://instagram.com/xxx',
+    googleApiKey: 'AIzaXXXXXXXXn_9ItJ58ogb80aXXXXXXXXX',
+    firebase: 'https://xxxxxx.firebaseapp.com'
+  },
+  pages: {
+    index: {
+      en: 'HOME',
+      ja: 'ホーム',
+      description: 'hogehufa'
+    },
+    salon: {
+      en: 'SALON',
+      ja: 'サロン・スタッフ紹介',
+      description: 'hogehufa'
+    },
+    service: {
+      en: 'SERVICE',
+      ja: 'メニュー案内',
+      description: 'hogehufa'
+    },
+    access: {
+      en: 'ACCESS',
+      ja: 'アクセス',
+      description: 'hogehufa'
+    }
+  }
+}

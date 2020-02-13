b5afe1fad0688dca1af9c5bb9437863fb827bdd4
soichiro-nitta <soic1226@gmail.com>
2020-02-14 00:21:53 +0900

    chore(package.json, yarn.lock, src/pages/pixi.tsx): pixi.jsを追加

--- src/pages/pixi.tsx
+++ src/pages/pixi.tsx
@@ -1,25 +1,38 @@
-// (1) import層
 import React from 'react'
 import styled from 'styled-components'
 import Link from 'next/link'
-// (2) Types層
+let PIXI: any
+if (typeof window !== `undefined`) {
+  PIXI = require('pixi.js')
+}
+
 type ContainerProps = {}
 type Props = {
   className: string
 } & ContainerProps
-// (3) DOM層
+
 const Component: React.FC<Props> = props => (
   <div className={props.className}>
     pixi.tsx
-    <Link href="/">to index.tsx</Link>
+    <Link href="/">
+      <a>to index.tsx</a>
+    </Link>
+    <Link href="/post/2">
+      <a>to post 2</a>
+    </Link>
   </div>
 )
-// (4) Style層
+
 const StyledComponent = styled(Component)`
   color: red;
 `
-// (5) Container層
+
 const Container: React.FC<ContainerProps> = props => {
+  React.useEffect(() => {
+    const app = new PIXI.Application()
+    document.body.appendChild(app.view)
+  }, [])
+
   return <StyledComponent {...props} className="pixi" />
 }
 

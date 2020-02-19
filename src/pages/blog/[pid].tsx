752754e4d7b74cc04a96f49029a3f7183a9b6f5a
soichiro-nitta <soic1226@gmail.com>
2020-02-20 03:29:41 +0900

    feat(src/pages/): unstable_getStaticPropsとunstable_getStaticPathsを追加

--- src/pages/blog/[pid].tsx
+++ src/pages/blog/[pid].tsx
@@ -9,3 +9,14 @@ const Post = () => {
 }
 
 export default Post
+
+// eslint-disable-next-line @typescript-eslint/camelcase
+export const unstable_getStaticProps = async () => {
+  return {}
+}
+
+// eslint-disable-next-line @typescript-eslint/camelcase
+export const unstable_getStaticPaths = async () => {
+  // Return our list of blog posts to prerender
+  return ['/blog/1', '/blog/2']
+}
